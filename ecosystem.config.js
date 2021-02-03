0b3a7ae42a4787f98f7f1f355b6325894c54265e
heypam68 <heypam68@gmail.com>
2021-02-03 07:36:41 +0000

    update config

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+          apps: [
+           {
+           name: 'library',
+           script: 'npm start'
+           }
+          ]
+};
--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  // baseURL: 'http://localhost:5000/api/v1',
-  baseURL: 'https://library-dw18lsfsq.herokuapp.com/api/v1',
+  baseURL: 'http://10.20.3.48:5000/api/v1',
+  //baseURL: 'https://library-dw18lsfsq.herokuapp.com/api/v1',
 });
 
 export const setToken = (token) => {
